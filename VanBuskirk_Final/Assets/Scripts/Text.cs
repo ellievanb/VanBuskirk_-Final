0f0159fd4811fc921385bc1e706ab9ada1ca522f
ellievanb <54558569+ellievanb@users.noreply.github.com>
2020-04-20 13:40:07 -0400

    save

---
+++ VanBuskirk_Final/Assets/Scripts/Text.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    public GameObject objectToDisable;
+    public static bool disabled = false;
+
+    private void Start()
+    {
+        objectToDisable.SetActive(false);
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("Clicked!");
+        StartCoroutine(Open());
+    }
+
+
+    IEnumerator Open()
+    {
+
+        objectToDisable.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        objectToDisable.SetActive(false);
+    }
+}
