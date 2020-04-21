f6ba911307a9eea61df908593d50af184c82df90
ellievanb <54558569+ellievanb@users.noreply.github.com>
2020-04-21 15:03:35 -0400

    save w/ issues

---
+++ VanBuskirk_Final/Assets/Scripts/SelectedFlash.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedFlash : MonoBehaviour
+{
+    public GameObject selectedObject;
+    public int redCol;
+    public int blueCol;
+    public int greenCol;
+    public bool lookingAtObject = false;
+    public bool flashingIn = true;
+    public bool startedFlashing = false;
+
+    void Update()
+    {
+        if (lookingAtObject == true)
+        {
+            selectedObject.GetComponent<Renderer>().material.color = new Color32((byte)redCol, (byte)greenCol, (byte)blueCol, 255);
+        }
+    }
+
+    void OnMouseOver()
+    {
+        selectedObject = GameObject.Find(CastingToObj.selectedObject);
+        lookingAtObject = true;
+        if(startedFlashing == false)
+        {
+            startedFlashing = true;
+            StartCoroutine(FlashObject());
+        }
+    }
+
+    void OnMouseExit()
+    {
+        startedFlashing = false;
+        lookingAtObject = false;
+        StopCoroutine(FlashObject());
+        selectedObject.GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 255);
+    }
+
+    IEnumerator FlashObject()
+    {
+        while(lookingAtObject == true)
+        {
+            yield return new WaitForSeconds(0.05f);
+            if (flashingIn == true)
+            {
+                if(blueCol <= 30)
+                {
+                    flashingIn = false;
+                }
+                else
+                {
+                    redCol -= 25;
+                    greenCol -= 25;
+                }
+            }
+            if (flashingIn == false)
+            {
+                if (blueCol >= 250)
+                {
+                    flashingIn = true;
+                }
+                else
+                {
+                    redCol += 25;
+                    greenCol += 25;
+                }
+            }
+        }
+    }
+}
