08434f47b255584e8f905595e22c570b1e2ca008
ellievanb <54558569+ellievanb@users.noreply.github.com>
2020-04-30 03:32:20 -0400

    attempts to add game ending

---
+++ VanBuskirk_Final/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public Canvas exitBackgroundImageCanvas;
+
+    bool m_IsPLayerAtExit;
+    float m_Timer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPLayerAtExit = true;
+        }
+    }
+
+    void Update()
+    {
+        if(m_IsPLayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel ()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvas.alpha = m_Timer / fadeDuration;
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
